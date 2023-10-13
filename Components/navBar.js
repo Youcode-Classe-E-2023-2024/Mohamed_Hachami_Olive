9e84bc8d4a836ec8ff0e445145eef90d6d290ace
MedHachami <moha.hach.9@gmail.com>
2023-10-13 20:58:49 +0200

    Update navBar.js

--- Components/navBar.js
+++ Components/navBar.js
@@ -1,15 +1,16 @@
 function generateNavbar() {
     return `
     <nav class="nav-container">
-        <a href="../index.html"><img src="./assets/logo_Olive.png" alt="logo" srcset="" class="logo-img"></a>
-        <ul class="nav-items">
-            <li class="nav-item"><a href="../index.html" class="nav-link">Home</a></li>
-            <li class="nav-item"><a href="../olivier.html" class="nav-link">L'olivier</a></li>
-            <li class="nav-item"><a href="../olive-maroc.html" class="nav-link">Olivier-maroc</a></li>
-            <li class="nav-item"><a href="../livres.html" class="nav-link">Livres</a></li>
-            <button class="join-btn" ><a href="../contact.html" >Contact Us</a></button>
-        </ul>
+    <a href="./index.html"><img src="./assets/logo_Olive.png" alt="logo" srcset="" class="logo-img"></a>
+    <ul class="nav-items">
+        <li class="nav-item"><a href="./" class="nav-link home">Home</a></li>
+        <li class="nav-item"><a href="./olivier.html" class="nav-link olivier">L'olivier</a></li>
+        <li class="nav-item"><a href="./olive-maroc.html" class="nav-link">Olivier-maroc</a></li>
+        <li class="nav-item"><a href="./livres.html" class="nav-link">Livres</a></li>
+        <button class="join-btn"><a href="./contact.php">Contact Us</a></button>
+    </ul>
     </nav>
+
     `;
 }
 
